f3aa3640706d20a03c698ecfe5b5e493e925ce02
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-22 11:39:18 +0530

    basic: version added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/krishpranav/goblockchain
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	version := "Version: 1.0"
+	fmt.Println("Go blockchain version: ", version)
+}
